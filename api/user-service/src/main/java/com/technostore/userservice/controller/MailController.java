f8b65f4a859353223cd216a4ee8a299c2a7e5918
Anastasia Molchanova <inastianice@yandex.ru>
2023-11-24 23:11:00 +0300

    add endpoint /send-code-for-confirmation-account

---
+++ api/user-service/src/main/java/com/technostore/userservice/controller/MailController.java
@@ -0,0 +1,38 @@
+package com.technostore.userservice.controller;
+
+import javax.mail.MessagingException;
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.technostore.userservice.service.MailService;
+import com.technostore.userservice.utils.AppError;
+
+@RestController
+public class MailController {
+
+    @Autowired
+    private MailService mailService;
+
+    @RequestMapping(path = "/send-code-for-confirmation-account", method = RequestMethod.GET, consumes = "application/json", produces = "application/json")
+    public ResponseEntity<?> sendConfirmationAccountCode(@RequestParam Long id) {
+        try {
+            mailService.sendConfirmationCode(id);
+        } catch (MessagingException e) {
+            return new ResponseEntity<>(
+                    new AppError(HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                            "Error sending code."), HttpStatus.INTERNAL_SERVER_ERROR);
+        } catch (EntityNotFoundException exception) {
+            return new ResponseEntity<>(
+                    new AppError(HttpStatus.NOT_FOUND.value(),
+                            "User with id " + id + " does not exist."), HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
