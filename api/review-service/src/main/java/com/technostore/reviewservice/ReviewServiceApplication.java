05a753e83a1db51331c5f84b71d85c554894a8a8
usanov-a <iambreaddd@mail.ru>
2023-11-25 16:02:20 +0300

    add review service

---
+++ api/review-service/src/main/java/com/technostore/reviewservice/ReviewServiceApplication.java
@@ -0,0 +1,25 @@
+package com.technostore.reviewservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RefreshScope
+public class ReviewServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewServiceApplication.class, args);
+    }
+
+    @LoadBalanced
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+}
