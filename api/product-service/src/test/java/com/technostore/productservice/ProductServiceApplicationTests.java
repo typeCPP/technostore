ac3b3fca765448012cb569a0fb20676c69b67db0
Anastasia Molchanova <inastianice@yandex.ru>
2023-11-25 14:14:48 +0300

    Add product service

--- api/product-service/src/test/java/com/technostore/productservice/ProductServiceApplicationTests.java
+++ api/product-service/src/test/java/com/technostore/productservice/ProductServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.technostore.userservice;
+package com.technostore.productservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class UserServiceApplicationTests {
+class ProductServiceApplicationTests {
 
     @Test
     void contextLoads() {
