54231fa8b3628343cafbb453d33fc53b766ae9ff
usanov-a <iambreaddd@mail.ru>
2023-11-25 15:42:29 +0300

    add get /popular-categories endpoint

---
+++ api/product-service/src/main/java/com/technostore/productservice/repository/CategoryRepository.java
@@ -0,0 +1,12 @@
+package com.technostore.productservice.repository;
+
+import java.util.Optional;
+
+import com.technostore.productservice.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findByName(String name);
+}
---
+++ api/product-service/src/main/java/com/technostore/productservice/service/CategoryServiceImpl.java
@@ -0,0 +1,30 @@
+package com.technostore.productservice.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import com.technostore.productservice.model.Category;
+import com.technostore.productservice.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+    @Autowired
+    CategoryRepository categoryRepository;
+    @Override
+    public List<Category> getPopularCategories() {
+        Set<String> categoriesToFind = Set.of("Ноутбуки", "Планшеты", "Фототехника");
+        List<Category> result = new ArrayList<>();
+
+        for (String name : categoriesToFind) {
+            Optional<Category> categoryOptional = categoryRepository.findByName(name);
+            categoryOptional.ifPresent(result::add);
+        }
+
+        return result;
+    }
+}
