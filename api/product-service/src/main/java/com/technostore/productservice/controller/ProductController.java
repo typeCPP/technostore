15243b3b70b3f681e92693af62d1fa4259645951
Anastasia Molchanova <inastianice@yandex.ru>
2023-11-28 11:37:07 +0300

    fix imports

--- api/product-service/src/main/java/com/technostore/productservice/controller/ProductController.java
+++ api/product-service/src/main/java/com/technostore/productservice/controller/ProductController.java
@@ -11,18 +11,23 @@ import java.util.Scanner;
 import javax.persistence.EntityNotFoundException;
 import javax.servlet.http.HttpServletRequest;
 
-import com.technostore.productservice.dto.ProductDto;
-import com.technostore.productservice.model.Product;
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpClientErrorException;
 
 import com.technostore.productservice.service.CategoryService;
+import com.technostore.productservice.dto.ProductDto;
 import com.technostore.productservice.dto.SortType;
 import com.technostore.productservice.service.ProductService;
 import com.technostore.productservice.service.client.UserRestTemplateClient;
