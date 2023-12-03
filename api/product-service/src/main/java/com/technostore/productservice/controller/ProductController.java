a5b866db73105052fb0697acd831431eed346695
Anastasia Molchanova <inastianice@yandex.ru>
2023-12-03 04:10:10 +0300

    Correct handle 403 error

--- api/product-service/src/main/java/com/technostore/productservice/controller/ProductController.java
+++ api/product-service/src/main/java/com/technostore/productservice/controller/ProductController.java
@@ -18,6 +18,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -138,5 +139,15 @@ public class ProductController {
 
         scanner.close();
         return resultList;
+    }
+
+    @ExceptionHandler(HttpClientErrorException.class)
+    public ResponseEntity<AppError> handleException(HttpClientErrorException e) {
+        if (e instanceof HttpClientErrorException.Forbidden) {
+            return new ResponseEntity<>(
+                    new AppError(HttpStatus.FORBIDDEN.value(),
+                            e.getMessage()), HttpStatus.FORBIDDEN);
+        }
+        throw e;
     }
 }
