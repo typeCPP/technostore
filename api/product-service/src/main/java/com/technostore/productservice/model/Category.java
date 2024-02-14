d0b6096ba1f77c49c7b6b80ce95cdeaa68115338
usanov-a <iambreaddd@mail.ru>
2024-02-14 20:41:26 +0300

    add product service tests

--- api/product-service/src/main/java/com/technostore/productservice/model/Category.java
+++ api/product-service/src/main/java/com/technostore/productservice/model/Category.java
@@ -6,6 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -15,6 +16,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Category {
 
     @Id
--- api/product-service/src/main/java/com/technostore/productservice/model/Product.java
+++ api/product-service/src/main/java/com/technostore/productservice/model/Product.java
@@ -3,6 +3,7 @@ package com.technostore.productservice.model;
 import javax.persistence.*;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,6 +13,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Product {
 
     @Id
