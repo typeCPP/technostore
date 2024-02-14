7e350af6e5d19e1e2722fa0cc10464daebb152e0
usanov-a <iambreaddd@mail.ru>
2024-02-14 19:11:39 +0300

    fix order service build

--- api/order-service/src/main/java/com/technostore/dto/ProductDto.java
+++ api/order-service/src/main/java/com/technostore/dto/ProductDto.java
@@ -2,10 +2,14 @@ package com.technostore.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 @Builder
 public class ProductDto {
     private long id;
