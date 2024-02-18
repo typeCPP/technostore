4c19343cd0a115bacb4ba12012b381b033bba56d
Anastasia Molchanova <inastianice@yandex.ru>
2024-02-18 07:38:24 +0300

    Delete unused setters

--- api/product-service/src/main/java/com/technostore/productservice/model/ProductPopularity.java
+++ api/product-service/src/main/java/com/technostore/productservice/model/ProductPopularity.java
@@ -5,11 +5,9 @@ import javax.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProductPopularity {
