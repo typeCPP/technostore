b85116bb4fad46bb2ac0453d30f2604c6b405eb9
Alena Ryzhova <alena-alena-2002@yandex.ru>
2024-02-24 02:05:33 +0300

    Create URL.kt

---
+++ client/app/src/androidTest/java/com/technostore/network/utils/URL.kt
@@ -0,0 +1,9 @@
+package com.technostore.network.utils
+
+object URL {
+    const val BASE_URL = "http://10.0.2.2:8080/"
+    const val USER_SERVICE_BASE_URL = "user-service"
+    const val PRODUCT_SERVICE_BASE_URL = "product-service"
+    const val ORDER_SERVICE_BASE_URL = "order-service"
+    const val REVIEW_SERVICE_BASE_URL = "review-service"
+}
