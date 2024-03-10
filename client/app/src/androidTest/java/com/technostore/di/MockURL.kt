2a20725388dc2a6f74282be141b6c8588dcf48d6
Alena Ryzhova <alena-alena-2002@yandex.ru>
2024-03-10 15:30:10 +0300

    Revert "Change android urls, change test url"
    This reverts commit d7631322122536c37302f0e8acf3b927e07ff63c.

--- client/app/src/androidTest/java/com/technostore/di/MockURL.kt
+++ client/app/src/androidTest/java/com/technostore/di/MockURL.kt
@@ -1,8 +1,8 @@
 package com.technostore.di
 
 object MockURL {
-    const val USER_SERVICE_BASE_URL = "http://10.0.2.2:8094/"
-    const val PRODUCT_SERVICE_BASE_URL = "http://10.0.2.2:8092/"
+    const val USER_SERVICE_BASE_URL = "http://10.0.2.2:8084/"
+    const val PRODUCT_SERVICE_BASE_URL = "http://10.0.2.2:8082/"
     const val ORDER_SERVICE_BASE_URL = "http://10.0.2.2:8086/"
-    const val REVIEW_SERVICE_BASE_URL = "http://10.0.2.2:8093/"
+    const val REVIEW_SERVICE_BASE_URL = "http://10.0.2.2:8083/"
 }
